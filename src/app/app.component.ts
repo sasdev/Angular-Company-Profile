7cf7811f8a66e67c5ad9e7d463296a2913b979e5
Sasdev <wentzelheinrich@gmail.com>
2020-05-03 18:28:16 +0200

    Made changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { MatMenuModule } from '@angular/material/menu';
-import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
