2bd114e500ef3be2ce4b5d2587959604281c370c
sasdev <wentzelheinrich@gmail.com>
2020-08-16 10:13:09 +0200

    1 file modified

--- src/app/pages/companies/mock-companies.ts
+++ src/app/pages/companies/mock-companies.ts
@@ -1,12 +1,12 @@
 import {Company} from './company'
 
 export const COMPANIES: Company[] = [
-  {id: 1, name: 'test1'},
-  {id: 2, name: 'test2'},
-  {id: 3, name: 'test3'},
-  {id: 4, name: 'test4'},
-  {id: 5, name: 'test5'},
-  {id: 6, name: 'test6'},
-  {id: 7, name: 'test7'},
-  {id: 8, name: 'test8'},
+  {id: 1, name: 'test1', content: 'I am the content for 1'},
+  {id: 2, name: 'test2', content: 'I am the content for 2'},
+  {id: 3, name: 'test3', content: 'I am the content for 3'},
+  {id: 4, name: 'test4', content: 'I am the content for 4'},
+  {id: 5, name: 'test5', content: 'I am the content for 5'},
+  {id: 6, name: 'test6', content: 'I am the content for 6'},
+  {id: 7, name: 'test7', content: 'I am the content for 7'},
+  {id: 8, name: 'test8', content: 'I am the content for 8'},
 ];
